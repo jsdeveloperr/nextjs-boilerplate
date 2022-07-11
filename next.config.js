ea74b4a8a65c7d347ced5041f397b8e175c103de
Abdulnasır Olcan <abdulnasirolcan@Abdulnasr-Olcan-MacBook-Pro.local>
2022-07-11 23:09:20 +0300

    🚀 preact removed

--- next.config.js
+++ next.config.js
@@ -12,21 +12,11 @@ module.exports = withBundleAnalyzer({
   basePath: '',
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { dev, isServer }) => {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
-
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
 
     return config
   },
